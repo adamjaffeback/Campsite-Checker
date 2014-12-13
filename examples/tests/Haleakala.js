5c26a023ed86aadb4b6d9afb2994897ef0adf658
Adam Back <adb33@email.arizona.edu>
2014-12-13 13:23:34 -0800

    Trying different approach, directly to cabin page."

--- examples/tests/Haleakala.js
+++ examples/tests/Haleakala.js
@@ -1,15 +1,27 @@
 module.exports = {
   "Look for available cabins in Haleakala NP, Hawai'i" : function (browser) {
     browser
-      .url("http://www.recreation.gov")
+      // .url("http://www.recreation.gov")
+      // .waitForElementVisible('body', 1000)
+      // .setValue('input[id=locationCriteria]', 'HALEAKALA NATIONAL PARK')
+      // .click('select[id=interest]')
+      // .click('option[value=permit]')
+      // .submitForm('form[id=unifSearchForm]')
+      // .waitForElementVisible('body', 1000)
+      // .click('option[value=permit]')
+      // .waitForElementVisible('body', 1000)
+      // .click('a[id=suggestion_1_2751]')
+      // .waitForElementVisible('body', 1000)
+      // .click('#check_avail_panel_NRSO112739 a.first')
+      .url('http://www.recreation.gov/permits/Haleakala_National_Park_Cabin_Permits/r/wildernessAreaDetails.do?page=detail&contractCode=NRSO&parkId=112739')
       .waitForElementVisible('body', 1000)
-      .setValue('input[id=locationCriteria]', 'Haleakala National Park')
-      .click('select[id=interest]')
-      .waitForElementVisible('optgroup[label=Reservable Activities]', 250)
-      .click('option[value=permit]')
+      .click('select[id=permitTypeId]')
+      .click('option[value=\'1451140610\']')
+      .waitForElementVisible('#entryStartDate', 5000)
+      .setValue('input[id=entryStartDate]', '01/25/2015')
+      .setValue('input[id=lengthOfStay]', '1')  
       .submitForm('form[id=unifSearchForm]')
-      .waitForElementVisible('body', 1000)
-      .assert.elementPresent('a[title=Haleakala National Park (Cabin Permits)]')
+      // .waitForElementVisible('body', 5000)
       .end();
   }
 };
