6a8ae3f256946e800645dcc576757ccb381efbf8
Adam Back <adb33@email.arizona.edu>
2014-12-14 22:21:26 -0800

    Removed protractor. Also removed examples folder, moving items out into main directory.

--- conf.js
+++
@@ -1,9 +0,0 @@
-exports.config = {
-  seleniumAddress: 'http://localhost:4444/wd/hub',
-  specs: ['specs.js'],
-  onPrepare: function() {
-    global.isAngularSite = function(flag) {
-      browser.ignoreSynchronization = !flag;
-    }     
-  }
-};
