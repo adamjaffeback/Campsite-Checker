5c3b4d608f54f4b2d0acafd8b75e8ef8d2c0a9cf
Adam Back <adb33@email.arizona.edu>
2014-12-14 22:11:09 -0800

    Form submitting and moving to availability page.

--- examples/tests/Haleakala.js
+++ examples/tests/Haleakala.js
@@ -4,21 +4,14 @@ module.exports = {
 
       .url('http://www.recreation.gov/permits/Haleakala_National_Park_Cabin_Permits/r/wildernessAreaDetails.do?page=detail&contractCode=NRSO&parkId=112739')
       .waitForElementVisible('body', 1000)
-      .click('select[id=permitTypeId]')
-      .keys(['\uE015', '\uE006'])
-      // .execute('document.getElementById(\'permitTypeId\').selectedIndex=1')
-      // .execute('document.getElementById(\'permitTypeId\').options[1].change()')
-      // .execute('document.getElementById(\'permitTypeId\').submit()')
-      // .moveToElement('select[id=permitTypeId]', 10, 10)
-      // ^tried to hover
-      // .waitForElementVisible('#permitTypeId option[value=1451140610]', 4000)
-      // ^does not pass
-      // .setValue('#permitTypeId.options[1].selected', 'true')
-      // .click('select:nth-child(2)')
-      // .click('select option[value=1451140610]')
-      // .click('#permitTypeId option[value=1451140610]')
-      // .click('#permitTypeId:nth-child(2)')
+      
+      .click('select[id=permitTypeId]') // click dropdown, <select> element
+      .keys(['\uE015', '\uE006']) // arrow down, enter
       .waitForElementVisible('#entryStartDate', 5000)
+      .setValue('input[id=entryStartDate]', '01/25/2015')
+      .setValue('input[id=lengthOfStay]', '1')  
+      .submitForm('form[id=unifSearchForm]')
+      .waitForElementVisible('body', 5000) // wait for availability page to load
       .end();
 
 
@@ -29,12 +22,7 @@ module.exports = {
 
 
 
-      // after clicking on permit type:
 
-      // .setValue('input[id=entryStartDate]', '01/25/2015')
-      // .setValue('input[id=lengthOfStay]', '1')  
-      // .submitForm('form[id=unifSearchForm]')
-      // .waitForElementVisible('body', 5000)
   }
 };
 
