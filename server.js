96e5e0c14cf2e779a19f88f1531f806c33b597c4
Adam Back <adb33@email.arizona.edu>
2014-12-15 23:39:08 -0800

    Set server to run every 10 minutes.

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var child_process = require('child_process');
 
 //run tests every 10 minutes (1000 * 60 * 10)
-setTimeout(function() {
+setInterval(function() {
   child_process.exec('npm test');
-}, 1000);
+}, (1000 * 60 * 10));
