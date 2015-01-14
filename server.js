53a2996248e0e82512217c56bcdeba78fd4fdda7
Adam Back <adb33@email.arizona.edu>
2015-01-13 21:58:48 -0800

    Server will log every time the script is run.

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
 var child_process = require('child_process');
+var fs = require('fs');
 
 //run tests every 10 minutes (1000 * 60 * 10)
 setInterval(function() {
-  child_process.exec('npm test');
+  fs.appendFile
 }, (1000 * 60 * 10));
+
+var runTests = function() {
+  var currentTime = new Date();
+
+  fs.appendFile('runlog.txt', currentTime.toString(), function(error) {
+    if(error) {
+      console.error(error);
+    }
+  });
+
+  child_process.exec('npm test');
+};
