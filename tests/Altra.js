4c04e3023e6e056d30080f4fb16ef7064a214404
Adam Back <adb33@email.arizona.edu>
2015-11-22 17:04:56 -0800

    (feat) Altra checks correctly.

--- tests/Altra.js
+++ tests/Altra.js
@@ -50,7 +50,7 @@ module.exports = {
     browser
 
       // navigate to main page
-      .url('http://http://pro.arcteryx.com/')
+      .url('http://pro.arcteryx.com/')
       .waitForElementVisible('body', 1000)
 
       // log in
@@ -59,14 +59,21 @@ module.exports = {
       .click( '#header > div > div > form > button' )
 
       // click beyond foot sizing page
+      .waitForElementVisible( '#lblMessage', 1000 )
       .click( '#btnOkay' )
 
       // click 'Outdoor Pro Product'
+      .waitForElementVisible( '#divPromotionLabel', 1000 )
       .click( '#divOrderTypeOptions > div:nth-child(1) > a:nth-child(1) > img:nth-child(3)' )
 
       // enter in search and go
+      .waitForElementVisible( '#txtSearch', 1000 )
       .setValue( '#txtSearch', 'ALTRA 75 BACKPACK' )
       .click( '#btnSearch' )
+
+      // confirm on correct page
+      .waitForElementVisible( '#divProduct > span', 1000 )
+      .assert.containsText( '#divProduct > span', 'ALTRA 75 BACKPACK MEN\'S' )
 
       // check the src tag for 'out of stock'
       .getAttribute( '#divProduct > table > tbody > tr > td > div:nth-child(2) > a > img', 'src', function( result ) {
