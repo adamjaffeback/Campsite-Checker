2ffe2bf4f18fa9a676f6d655bffefbf061a56e89
Adam Back <adb33@email.arizona.edu>
2015-11-22 17:09:49 -0800

    (refactor) Arc test runs every 6 hours.

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 var child_process = require('child_process');
 var fs = require('fs');
 
-//run tests every 10 minutes (1000 * 60 * 10)
+//run tests every 6 hours (1000 * 60 * 10)
+// 1000 * 60 - one minute
+// * 60 - sixty minutes, one hour
+// * 6 - 6 hours
 setInterval(function() {
   runTests();
-}, (1000 * 60 * 10));
+}, ( 1000 * 60 * 60 * 6 ));
 
 var runTests = function() {
   var currentTime = new Date();
